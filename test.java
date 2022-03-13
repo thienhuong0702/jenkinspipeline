8230f0ad49ca7911c0f1f36ec50a770e3df2bdbd
thienhuong0702 <56148108+thienhuong0702@users.noreply.github.com>
2022-03-13 15:49:46 +0700

    Delete test.java

--- test.java
+++
@@ -1,7 +0,0 @@
-class test{
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("HuongPhan-6656");
-        System.out.println("Test send email");
-    }
-}
